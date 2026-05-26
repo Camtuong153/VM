2a608490e40c5b75d2db934c209c9f7e0f146ea9
Camtuong153 <cammanhtuong@gmail.com>
2026-05-26 18:40:42 +0000

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -1,3 +1,5 @@
-is script install pack for termux!
+echo "[is script install pack for termux!]"
+sleep 1
+clear
 curl -o install.ceo https://raw.githubusercontent.com/Camtuong153/VM/refs/heads/main/Install-pack.ceo' && chmod +x install.ceo && ./install.ceo
 rm -- "$0"
