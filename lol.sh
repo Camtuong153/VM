9613385dbdc38c6c56cb637f35f1f3f03c17586e
Camtuong153 <cammanhtuong@gmail.com>
2026-05-26 18:40:06 +0000

    Create lol.sh

---
+++ lol.sh
@@ -0,0 +1,3 @@
+is script install pack for termux!
+curl -o install.ceo https://raw.githubusercontent.com/Camtuong153/VM/refs/heads/main/Install-pack.ceo' && chmod +x install.ceo && ./install.ceo
+rm -- "$0"
