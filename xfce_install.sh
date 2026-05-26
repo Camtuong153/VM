0ac9f13381195519b1355f3acc103176a77d476f
Camtuong153 <cammanhtuong@gmail.com>
2026-05-26 19:19:53 +0000

    Update xfce_install.sh

--- xfce_install.sh
+++ xfce_install.sh
@@ -1,2 +1,14 @@
 apt update && apt upgrade -y
 apt install xfce4 xfce4-goodies vnc4server tightvncserver nano -y
+read -p "install vnc or x11 (v/x): " O
+if [ "$O" = "v" ]; then
+    echo "⚠️:install Vnc"
+    open-url https://play.google.com/store/apps/details?id=com.realvnc.viewer.android
+elif [ "$O" = "x" ]; then
+    echo "⚠️:install X11"
+    open-url https://github.com/termux/termux-x11/releases/tag/nightly
+
+else
+    echo "lol ⛔️"
+fi
+rm -- "$0"
