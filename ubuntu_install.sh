729fca3a9ab68f83d5ff0c79620a2b0ce9c601a5
Camtuong153 <cammanhtuong@gmail.com>
2026-05-26 18:57:36 +0000

    Create ubuntu_install.sh

---
+++ ubuntu_install.sh
@@ -0,0 +1,15 @@
+clear
+echo ========================================================
+echo   install ubuntu started , you don't not press any key!
+echo ========================================================
+sleep 4
+clear
+echo installpack...
+apt update && apt upgrade -y
+pkg install proot-distro -y
+proot-distro install ubuntu
+echo install pack complete
+sleep 2
+clear
+echo next you paste code: 
+proot-distro login ubuntu
