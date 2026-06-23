e18d2e1540623073377cab05805743e3f2211ad1
Camtuong153 <cammanhtuong@gmail.com>
2026-06-23 23:55:08 +0700

    Update setup_user.sh

--- setup_user.sh
+++ setup_user.sh
@@ -3,8 +3,8 @@ mkdir -p /sdcard/VM
 chmod +rwx /sdcard/VM
 
 cat << eof > ~/startxp.sh
-am start -n com.realvnc.viewer.android/.MainActivity
-qemu-system-x86_64 -usb -device usb-tablet -device virtio-keyboard-device -cpu qemu32 -smp sockets=1,cores=1,threads=1 -m 512M -hda "/storage/emulated/0/VM/Windows XP x64/XP64VL.qcow2" -vga std -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi -boot c -rtc clock=rt -monitor stdio -display sdl,gl=on -vnc :2 -name startxp -uuid 05e7c431-9513-d856-0276-7859640ce042
+qemu-system-x86_64 -usb -device usb-tablet -device virtio-keyboard-device -cpu qemu32 -smp sockets=1,cores=1,threads=1 -m 512M -hda "/storage/emulated/0/VM/Windows XP x64/XP64VL.qcow2" -vga std -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi -boot c -rtc clock=rt -monitor stdio -display sdl,gl=on -vnc :2 -name startxp -uuid 05e7c431-9513-d856-0276-7859640ce042 &
+termux-open-url vnc://127.0.0.1:5900
 eof
 chmod +rwx ~/startxp.sh
 wget -O /sdcard/VM/a.7z https://download2288.mediafire.com/ql3x0f1yhcxg7FTlsdUJbu9eNBXePqQV62394F5KppyknYDvPzKdMiFF3sxQqLJlL9DJMYofGJRa0vOFG4Qp-HPNU0kCTIeazh_7u85K0JcuY3etMAqdgp6vO528PVmA4NOWIqTJ9Bsyh14sbuS08EPb2DmPaXeg4n2-MYikneKNPQ/xuv54r2pf1lkgup/Windows+XP+x64.7z
